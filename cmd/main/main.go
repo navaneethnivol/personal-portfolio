24712104454fa35aa0090eb83cd849cf6759803c
Navaneeth Nivol <33155848+navaneethnivol@users.noreply.github.com>
2024-01-18 21:17:23 -0800

    basic website serving code added

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,17 +1,67 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("frontend/build"))
-	http.Handle("/", fs)
 
-	log.Println("Listening on :3000.")
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
+	// Global Vars
+	port := 3001
+
+	fileServer := http.FileServer(http.Dir("./frontend/build"))
+
+	// Database Connection
+
+	// const file string = "database.db"
+	// db, err := sql.Open("sqlite3", file)
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// defer db.Close()
+
+	// Routes
+	http.Handle("/", fileServer)
+	http.HandleFunc("ping", routeHandler)
+
+	fmt.Printf("starting server on port: %d", port)
+	if err := http.ListenAndServe(":"+strconv.Itoa(port), nil); err != nil {
 		log.Fatal(err)
 	}
+
+	// const create string = `
+	// 	CREATE TABLE IF NOT EXISTS activities (
+	// 	id INTEGER NOT NULL PRIMARY KEY,
+	// 	time DATETIME NOT NULL,
+	// 	description TEXT
+	// 	);
+	// `
+
+	// fs := http.FileServer(http.Dir("frontend/build"))
+	// http.Handle("/", fs)
+
+	// log.Println("Listening on :3000.")
+	// err := http.ListenAndServe(":3000", nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+}
+
+func routeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/ping" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "pong!")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/navaneethnivol/personal-portfolio
 
 go 1.20
+
+require github.com/mattn/go-sqlite3 v1.14.17 // indirect
